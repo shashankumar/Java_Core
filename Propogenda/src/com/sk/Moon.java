bf1de2d644acb71d03ab3ee95a2ee7ebee5f33f1
shashank <shashank@192.168.0.103>
2020-06-24 09:46:25 +0530

    This is moon file.

---
+++ Propogenda/src/com/sk/Moon.java
@@ -0,0 +1,39 @@
+package com.sk;
+
+public class Moon {
+
+	public static void main(String[] args) {
+		System.out.println("                          *				");
+		System.out.println("                       * *				");
+		System.out.println("                   * * *				");
+		System.out.println("                 * * *					");
+		System.out.println("               * * * *					");
+		System.out.println("             * * * *					");
+		System.out.println("           * * * * *					");
+		System.out.println("           * * * *					");
+		System.out.println("         * * * * *					");
+		System.out.println("         * * * * *					");
+		System.out.println("       * * * * * *					");
+		System.out.println("       * * * * * *					");
+		System.out.println("       * * * * * *					");
+		System.out.println("       * * * * * *					");
+		System.out.println("       * * * * * * *		               *			");
+		System.out.println("       * * * * * * *					");
+		System.out.println("       * * * * * * *					");
+		System.out.println("       * * * * * * * *					");
+		System.out.println("       * * * * * * * *					");
+		System.out.println("       * * * * * * * * *					");
+		System.out.println("         * * * * * * * * *				");
+		System.out.println("        * * *  * * * * * * *				");
+		System.out.println("           * * * * * * * * * *				");
+		System.out.println("             * * * * * * * * * *				");
+		System.out.println("             * * * * * * * * * * * *					");
+		System.out.println("               * * * * * * * * * * * * *					");
+		System.out.println("                   * * * * * * * * * * * * * * *           * *				");
+		System.out.println("                     * * * * * * * * * * * * * * * * * * *				");
+		System.out.println("                         * * * * * * * * * * * * * * *				");
+		System.out.println("                               * * * * * * * * *				");
+
+	}
+
+}
